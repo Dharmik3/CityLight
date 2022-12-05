193fdae6f5047c0daaeb4c1f7bb9f48f859dde40
Dharmik Patel <dharmikpatel.pepalla@gmail.com>
2022-12-05 13:14:45 +0530

    initial header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
-
+import Logo from '../img/logo.png'
+import { MdShoppingBasket } from 'react-icons/md'
+import Avatar from '../img/avatar.png'
 function Header() {
   return (
-    <div>Header</div>
+      <header className='w-screen fixed z-50  p-6 px-16'>
+         {/* desktop navigation */}
+          <div className='hidden md:flex h-full w-full items-center justify-between'>
+              <div className='flex items-center gap-2'>
+                  <img src={Logo} alt="logo" className='w-8 object-cover' />
+                  <p className='text-headingColor text-xl font-bold'>City</p>
+              </div>
+              <div className='flex items-center gap-8'>
+                    <ul className='flex items-center gap-8'>
+                        <li className='text-base text-textColor duration-100 transition-all ease-in-out hover:text-headingColor cursor-pointer'>Home</li>
+                        <li className='text-base text-textColor duration-100 transition-all ease-in-out hover:text-headingColor cursor-pointer'>Menu</li>
+                        <li className='text-base text-textColor duration-100 transition-all ease-in-out hover:text-headingColor cursor-pointer'>About Us</li>
+                        <li className='text-base text-textColor duration-100 transition-all ease-in-out hover:text-headingColor cursor-pointer'>Service</li>
+                    </ul>
+
+                    <div className='relative flex items-center justify-center'>
+                        <MdShoppingBasket className='text-textColor text-2xl  cursor-pointer ' />
+                        <div className='absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
+                            <p className='text-xs text-white font-semibold'>2</p>
+                        </div>
+                    
+                  </div>
+                  <img src={ Avatar} alt="user profile img" className='w-10 min-w-[40px] min-h-[40px] drop-shadow-xl cursor-pointer'/>
+
+                </div>
+          </div>
+
+
+          {/* phone navigation */}
+          <div className='flex md:hidden w-full h-full'></div>
+      </header>
   )
 }
 
