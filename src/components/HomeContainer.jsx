c8ef685fbbd429a0d7ee67d1b88a64a3e9b51c77
Dharmik Patel <dharmikpatel.pepalla@gmail.com>
2022-12-06 19:36:57 +0530

    hero with set bg

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -14,11 +14,13 @@ import HeroBg from '../img/heroBg.png'
               <p className='text-[2.5rem] font-bold tracking-wide text-headingColor   lg:text-[4.3rem]'>The Fastest Delivery in <span className='text-orange-600 text-[3rem] lg:text-[5rem]'>Your City</span></p>
               <p className='text-base text-textColor text-center md:text-left md:w-[80%]'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident sed quisquam sapiente ullam nulla vel, quam totam eum a possimus, doloremque accusantium doloribus numquam perferendis porro itaque dolores iusto ratione.</p>
               <button type='button' className='bg-gradient-to-br from-orange-400 to-orange-500 w-full px-4 py-2 rounded-lg hover:shadow-lg transition-all duration-100 ease-in-out md:w-auto'>Order Now</button>
-             </div>
-             <div className='py-2 flex-1 flex items-center'>
-                 <img src={HeroBg} alt="hero-bg" className='lg:h-650 h-420 ml-auto w-full lg:w-full' />
-                 <div className='w-full h-full absolute flex items-center justify-center'></div>
-             </div>
+     </div>
+    <div className='py-2 flex-1 flex items-center'>
+        <img src={HeroBg} alt="hero-bg" className='lg:h-650 h-420 ml-auto w-full lg:w-auto' />
+                 <div className='w-full h-full absolute flex items-center justify-center'>
+                     
+                </div>
+     </div>
 </section>
   )
 }
