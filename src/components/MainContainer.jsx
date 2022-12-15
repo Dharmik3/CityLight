d5c36c321d5d223dc475f2dfa6f2e9f05d74d298
Dharmik Patel <dharmikpatel.pepalla@gmail.com>
2022-12-15 19:58:28 +0530

    cart ui

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { HomeContainer,FruitsBar,MenuContainer } from '../components'
+import { HomeContainer,FruitsBar,MenuContainer ,CartContainer} from '../components'
 
 const MainContainer = () => {
   
   return (
       <div className='flex flex-col w-full h-auto items-center justify-center'>
           <HomeContainer/>
-          <FruitsBar/>
-          <MenuContainer/>
+        <FruitsBar/>
+      <MenuContainer />
+      <CartContainer/>
     </div>
   )
 }
