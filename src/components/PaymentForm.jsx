5064f3f43f94330531b557fdee1bc7610f943091
Dharmik Patel <dharmikpatel.pepalla@gmail.com>
2023-01-30 21:18:20 +0530

    payment form with better UI

--- src/components/PaymentForm.jsx
+++ src/components/PaymentForm.jsx
@@ -1,77 +1,180 @@
-import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import axios from "axios";
-import React, { useState } from "react";
+// import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+// import axios from "axios";
+// import React, { useState } from "react";
 
-const CARD_OPTIONS = {
-  iconStyle: "solid",
-  style: {
-    base: {
-      iconColor: "#c4f0ff",
-      color: "black",
-      fontWeight: 500,
-      fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
-      fontSize: "16px",
-      fontSmoothing: "antialiased",
-      ":-webkit-autofill": { color: "#fce883" },
-      "::placeholder": { color: "gray" },
-    },
-    invalid: {
-      iconColor: "#ffc7ee",
-      color: "#ffc7ee",
-    },
-  },
-};
+// const CARD_OPTIONS = {
+//   iconStyle: "solid",
+//   style: {
+//     base: {
+//       iconColor: "#c4f0ff",
+//       color: "black",
+//       fontWeight: 500,
+//       fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
+//       fontSize: "16px",
+//       fontSmoothing: "antialiased",
+//       ":-webkit-autofill": { color: "#fce883" },
+//       "::placeholder": { color: "gray" },
+//     },
+//     invalid: {
+//       iconColor: "#ffc7ee",
+//       color: "#ffc7ee",
+//     },
+//   },
+// };
+
+// export default function PaymentForm() {
+//   const [success, setSuccess] = useState(false);
+//   const stripe = useStripe();
+//   const elements = useElements();
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     const { error, paymentMethod } = await stripe.createPaymentMethod({
+//       type: "card",
+//       card: elements.getElement(CardElement),
+//     });
+
+//     if (!error) {
+//       try {
+//         const { id } = paymentMethod;
+//         const response = await axios.post("http://localhost:4000/payment", {
+//           amount: 1000,
+//           id,
+//         });
+
+//         if (response.data.success) {
+//           console.log("Successful payment");
+//           setSuccess(true);
+//         }
+//       } catch (error) {
+//         console.log("Error", error);
+//       }
+//     } else {
+//       console.log(error.message);
+//     }
+//   };
+
+//   return (
+//     <>
+//       {!success ? (
+//         <form onSubmit={handleSubmit}>
+//           <fieldset className="FormGroup">
+//             <div className="FormRow">
+//               <CardElement options={CARD_OPTIONS} />
+//             </div>
+//           </fieldset>
+//           <button>Pay</button>
+//         </form>
+//       ) : (
+//         <div>
+//           <h2>
+//             Thank you for ordering from CityLight!
+//           </h2>
+//         </div>
+//       )}
+//     </>
+//   );
+// }
+
+
+import React, { useEffect, useState } from "react";
+import {
+  PaymentElement,
+  LinkAuthenticationElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
 
 export default function PaymentForm() {
-  const [success, setSuccess] = useState(false);
   const stripe = useStripe();
   const elements = useElements();
 
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (!stripe) {
+      return;
+    }
+
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    );
+
+    if (!clientSecret) {
+      return;
+    }
+
+    stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+      switch (paymentIntent.status) {
+        case "succeeded":
+          setMessage("Payment succeeded!");
+          break;
+        case "processing":
+          setMessage("Your payment is processing.");
+          break;
+        case "requires_payment_method":
+          setMessage("Your payment was not successful, please try again.");
+          break;
+        default:
+          setMessage("Something went wrong.");
+          break;
+      }
+    });
+  }, [stripe]);
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-      type: "card",
-      card: elements.getElement(CardElement),
+
+    if (!stripe || !elements) {
+      // Stripe.js has not yet loaded.
+      // Make sure to disable form submission until Stripe.js has loaded.
+      return;
+    }
+
+    setIsLoading(true);
+
+    const { error } = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        // Make sure to change this to your payment completion page
+        return_url: "http://localhost:3000",
+      },
     });
 
-    if (!error) {
-      try {
-        const { id } = paymentMethod;
-        const response = await axios.post("http://localhost:4000/payment", {
-          amount: 1000,
-          id,
-        });
-
-        if (response.data.success) {
-          console.log("Successful payment");
-          setSuccess(true);
-        }
-      } catch (error) {
-        console.log("Error", error);
-      }
+    // This point will only be reached if there is an immediate error when
+    // confirming the payment. Otherwise, your customer will be redirected to
+    // your `return_url`. For some payment methods like iDEAL, your customer will
+    // be redirected to an intermediate site first to authorize the payment, then
+    // redirected to the `return_url`.
+    if (error.type === "card_error" || error.type === "validation_error") {
+      setMessage(error.message);
     } else {
-      console.log(error.message);
+      setMessage("An unexpected error occurred.");
     }
+
+    setIsLoading(false);
+  };
+
+  const paymentElementOptions = {
+    layout: "tabs",
   };
 
   return (
-    <>
-      {!success ? (
-        <form onSubmit={handleSubmit}>
-          <fieldset className="FormGroup">
-            <div className="FormRow">
-              <CardElement options={CARD_OPTIONS} />
-            </div>
-          </fieldset>
-          <button>Pay</button>
-        </form>
-      ) : (
-        <div>
-          <h2>
-            Thank you for ordering from CityLight!
-          </h2>
-        </div>
-      )}
-    </>
+    <form id="payment-form" onSubmit={handleSubmit}>
+      <LinkAuthenticationElement
+        id="link-authentication-element"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <PaymentElement id="payment-element" options={paymentElementOptions} />
+      <button disabled={isLoading || !stripe || !elements} id="submit">
+        <span id="button-text">
+          {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
+        </span>
+      </button>
+      {/* Show any error or success messages */}
+      {message && <div id="payment-message">{message}</div>}
+    </form>
   );
 }
--- src/components/StripeContainer.jsx
+++ src/components/StripeContainer.jsx
@@ -1,17 +1,65 @@
-import { Elements } from "@stripe/react-stripe-js";
+// import { Elements } from "@stripe/react-stripe-js";
+// import { loadStripe } from "@stripe/stripe-js";
+// import React from "react";
+// import PaymentForm from "./PaymentForm";
+
+// const PUBLIC_KEY =
+//   "pk_test_51MVcrmSFMRurGeBZ16JSn5mu4j4k7kUgoBqjHx1UkL5fnBqKvDEXpQupB7pDs9jgvi4n7YVaS5kDQA3U7MoUwzSa001LWih2Yz";
+
+// const stripeTestPromise = loadStripe(PUBLIC_KEY);
+
+// export default function StripeContainer() {
+//   return (
+//     <Elements stripe={stripeTestPromise}>
+//       <PaymentForm />
+//     </Elements>
+//   );
+// }
+
+import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
-import React from "react";
+import { Elements } from "@stripe/react-stripe-js";
+
+// import CheckoutForm from "./CheckoutForm";
+import "./App.css";
 import PaymentForm from "./PaymentForm";
 
-const PUBLIC_KEY =
-  "pk_test_51MVcrmSFMRurGeBZ16JSn5mu4j4k7kUgoBqjHx1UkL5fnBqKvDEXpQupB7pDs9jgvi4n7YVaS5kDQA3U7MoUwzSa001LWih2Yz";
-
-const stripeTestPromise = loadStripe(PUBLIC_KEY);
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// This is your test publishable API key.
+const stripePromise = loadStripe(
+  "pk_test_51MVcrmSFMRurGeBZ16JSn5mu4j4k7kUgoBqjHx1UkL5fnBqKvDEXpQupB7pDs9jgvi4n7YVaS5kDQA3U7MoUwzSa001LWih2Yz"
+);
 
 export default function StripeContainer() {
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch("http://localhost:4000/payment", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: "stripe",
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
   return (
-    <Elements stripe={stripeTestPromise}>
-      <PaymentForm />
-    </Elements>
+    <div className="App">
+      {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <PaymentForm />
+        </Elements>
+      )}
+    </div>
   );
 }
